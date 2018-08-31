e760771c81a3695d924e7522d8e32adf2b605b49
weiwei-abc <244096589@qq.com>
2018-08-31 11:27:03 +0800

    weiwei-abc-180831

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,9 @@
+package com.taotao.service;
+
+import com.taotao.pojo.*;
+
+public interface ItemService {
+	
+	TbItem getItemById(long itemId);
+	
+}
---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -0,0 +1,23 @@
+package com.taotao.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.taotao.pojo.*;
+import com.taotao.service.*;
+
+@Controller
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+	
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getItemById(@PathVariable Long itemId) {
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
+	}
+}
